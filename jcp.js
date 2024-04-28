44e784778044c66d2226cd02dae57442d230dfc7
Toreyj95 <164526917+Toreyj95@users.noreply.github.com>
2024-04-28 11:12:40 -0400

    jcp

---
+++ jcp.js
@@ -0,0 +1,9 @@
+let partyLocation = "GARDEN"; // String 
+const ShiftValue = 3; //Number
+Number.isInteger(ShiftValue) //checks to see if its value is an integer
+let codedMesaage =""; //string 
+let DecodedMessaeg ="" ;//string 
+let isPartySafe = "false"; // Boolean
+
+
+
